294d9e82723d7f29bc8a324d1c9f26bf0eb76214
Guadalupe Camacho <gcamacho079@gmail.com>
2021-03-12 14:25:41 -0800

    Add link to blog in av

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,24 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import Logo from '../assets/logo.png';
 
 const Header = () => {
   return (
     <header className="header">
       <h1>
-        <img
-          src={Logo}
-          alt="Lupe Camacho"
-          width="200"
-          height="140"
-          title="Lupe Camacho"
-        />
+        <Link to={'/'}>
+          <img
+            src={Logo}
+            alt="Lupe Camacho"
+            width="200"
+            height="140"
+            title="Lupe Camacho"
+          />
+        </Link>
       </h1>
+      <nav>
+        <Link to="/blog">Blog</Link>
+      </nav>
     </header>
   );
 };
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,13 +2,15 @@ import React from 'react';
 //import Helmet from 'react-helmet';
 import Layout from '../components/Layout';
 import truncateText from '../utils/truncateText';
-import { graphql } from 'gatsby';
+import { graphql, Link } from 'gatsby';
 
 const SinglePostItem = ({ post }) => {
-  const { data } = post;
+  const { data, uid } = post;
   return (
     <article>
-      <h2>{data.title.text}</h2>
+      <h2>
+        <Link to={`/post/${uid}`}>{data.title.text}</Link>
+      </h2>
       <p>{data.date}</p>
       <p>{truncateText(data.body[0].primary.text.text)}</p>
     </article>
@@ -48,6 +50,7 @@ export const query = graphql`
             }
           }
         }
+        uid
       }
     }
   }
